e1ec77a716506bc3b75715916d497f200f64007e
vsuryapavan <suryavemuri@hotmail.com>
2017-02-15 18:43:58 -0800

    update

--- com/wip/training/exercise5/Calculator.java
+++ com/wip/training/exercise5/Calculator.java
@@ -2,33 +2,33 @@ package com.wip.training.exercise5;
 
 public class Calculator {
 
-  public void add(int num1, int num2) {
-    System.out.println("Addition = " + (num1 + num2));
+  public int add(int num1, int num2) {
+    return num1 + num2;
 
   }
 
-  public void mod(int num1, int num2) {
-    System.out.println("Modulus = " + (num1 % num2));
+  public int mod(int num1, int num2) {
+    return num1 % num2;
 
   }
 
-  public void sub(int num1, int num2) {
-    System.out.println("Substraction = " + (num1 - num2));
+  public int sub(int num1, int num2) {
+    return num1 - num2;
   }
 
 
-  public void multiply(int num1, int num2) {
-    System.out.println("Multiplication = " + (num1 * num2));
+  public int multiply(int num1, int num2) {
+    return num1 * num2;
 
   }
 
-  public void div(int num1, int num2) {
-    System.out.println("Division = " + (num1 / num2));
+  public int div(int num1, int num2) {
+    return num1 / num2;
 
   }
 
-  public void sq(int num1) {
-    System.out.println("Square = " + (num1 * num1));
+  public int sq(int num1) {
+    return num1 * num1;
 
   }
 
